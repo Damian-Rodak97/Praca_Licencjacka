aecdb1026acf5be6e68f0624e04619528bf1a028
Damian-Rodak97 <damian.rodak97a@gmail.com>
2019-10-08 21:11:13 +0200

    Create Project

---
+++ AdoptujZwierzaka/AdoptujZwierzaka/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdoptujZwierzaka.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

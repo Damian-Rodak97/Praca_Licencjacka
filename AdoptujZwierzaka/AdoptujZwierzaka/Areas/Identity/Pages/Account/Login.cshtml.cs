ad5b7b0c65caa79d7012f0312c258605d8bce6aa
DamianRodak <deny4000@o2.pl>
2019-11-16 11:41:09 +0100

    Improve login

--- AdoptujZwierzaka/AdoptujZwierzaka/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AdoptujZwierzaka/AdoptujZwierzaka/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -46,8 +46,6 @@ namespace AdoptujZwierzaka.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            public string Name { get; set; }
-            [Required]
             [EmailAddress]
             public string Email { get; set; }
 
--- AdoptujZwierzaka/AdoptujZwierzaka/Models/SeedData.cs
+++ AdoptujZwierzaka/AdoptujZwierzaka/Models/SeedData.cs
@@ -24,42 +24,40 @@ namespace AdoptujZwierzaka.Models
                         Name="Azor",
                         Description = "Pies ma 6 lat i jest bardzo aktywny.",
                         Category = "Pies",
-                        AddDate = DateTime.Parse("06/21/2017 17:22:16")
-                    },
+                                            },
                     new Pet
                     {
                         Name = "Inka",
                         Description = "Pies ma 3 lata i jest bardzo aktywna.",
                         Category = "Pies",
-                        AddDate = DateTime.Parse("08/18/2018 12:12:14")
+                        
                     },
                     new Pet
                     {
                         Name = "Perła",
                         Description = "Kotka ma 2 lata i jest bardzo aktywna.",
                         Category = "Kot",
-                        AddDate = DateTime.Parse("12/30/2011 09:33:26")
+                        
                     },
                     new Pet
                     {
                         Name = "Alex",
                         Description = "Pies ma 3 lata i jest bardzo aktywna.",
                         Category = "Pies",
-                        AddDate = DateTime.Parse("01/13/2015 19:32:14")
-                    },
+                                            },
                     new Pet
                     {
                         Name = "Cezar",
                         Description = "Kot ma rok i jest bardzo leniwy.",
                         Category = "Kot",
-                        AddDate = DateTime.Parse("03/12/2014 02:13:14")
+                        
                     },
                     new Pet
                     {
                         Name = "Kropka",
                         Description = "Pies ma roczek i jest bardzo leniwy.",
                         Category = "Pies",
-                        AddDate = DateTime.Parse("06/28/2017 13:12:13")
+                        
                     }
                     );
                 context.SaveChanges();

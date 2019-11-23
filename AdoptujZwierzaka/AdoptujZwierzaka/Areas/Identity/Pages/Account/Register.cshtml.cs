92ec258a9a518ebae207e6121a8ef0ce80f6f93f
Damian Rodak <damian.rodak97a@gmail.com>
2019-11-23 12:58:03 +0100

    Many changes and improvments

--- AdoptujZwierzaka/AdoptujZwierzaka/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AdoptujZwierzaka/AdoptujZwierzaka/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,10 +46,6 @@ namespace AdoptujZwierzaka.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [StringLength(maximumLength:10, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long", MinimumLength = 3)]
-            [Display(Name = "Name")]
-            public string Name { get; set; }
-            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -78,7 +74,7 @@ namespace AdoptujZwierzaka.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Name, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
